8d6d63ed86ce124c6d89cec42358890625876e9b
pavan0628 <pavan.cse.rymec@gmail.com>
2025-05-29 09:44:01 +0530

    [PAVAN]ADDED:Implemented regx for validating last name

--- src/main/java/com/brlab/regx/User.java
+++ src/main/java/com/brlab/regx/User.java
@@ -1,8 +1,27 @@
 package com.brlab.regx;
 
+import java.util.Scanner;
+
 public class User {
-    
-    public static void main(String[] args) {
-        System.out.println("Welcome to UserRegistration problem!");
+
+    public static boolean validateFirstName(String firstName)
+    {
+        return firstName.matches("^[A-Z][a-zA-Z]{2,}$");
+    }
+    public static boolean validateSecondName(String secondName)
+    {
+        return secondName.matches("^[A-Z][a-zA-Z]{2,}$");
+    }
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter First Name");
+        String firstName=scanner.nextLine();
+        System.out.println("Enter Last Name");
+        String lastName=scanner.nextLine();
+
+        System.out.println("First name having First character Upper and having minium 3 character "+validateFirstName(firstName));
+        System.out.println("Second name having First character Upper and having minium 3 character "+validateSecondName(lastName));
+
     }
 }
